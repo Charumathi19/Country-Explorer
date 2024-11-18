aa68487a39d435e8efc464a83f6207fba30f3252
Charumathi19 <charumurugan19@gmail.com>
2024-11-19 01:00:45 +0530

    Display Flag and Country Name

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,13 +1,37 @@
+import { HttpClient, HttpClientModule  } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  countries: { name: string; flag: string }[] = []; // Store country name and flag
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const apiKey = '514cb5fa37cb315a73418e89b99320c8'; 
+    const url = `https://api.countrylayer.com/v2/all?access_key=${apiKey}`;
+
+    this.http.get<any[]>(url).subscribe(
+      (response) => {
+        console.log(this.countries)
+        this.countries = response.map((country) => ({
+          name: country.name,
+          flag: country.flag
+        }));
+      },
+      (error) => {
+        console.error('Error fetching countries:', error);
+      }
+    );
+  }
+
+
 
 }
